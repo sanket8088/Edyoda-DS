56a855293369da9457710ce1ebe43489c795b929
SANKET <32214164+sanket8088@users.noreply.github.com>
2020-08-19 20:42:31 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,81 @@
+
+// Question 1 (Balance Paranthesis)
+const balancePara = (mStr) => {
+    stack = []
+    start = ["[", "{", "("]
+    end = ["]", "}", ")"]
+    for (i = 0; i < mStr.length; i++) {
+        if (start.includes(mStr[i])) {
+            stack.push(mStr[i])
+        }
+        else if (end.includes(mStr[i])) {
+            closeIndex = end.indexOf(mStr[i])
+            lastIndex = start.indexOf(stack[stack.length - 1])
+            if (closeIndex === lastIndex) {
+                stack.pop()
+            }
+            else {
+                return i + 1
+            }
+        }
+
+    }
+    if (stack.length == 0) {
+        return "Success"
+    }
+    else {
+        return mStr.indexOf(stack[stack.length - 1]) + 1
+    }
+
+}
+
+// console.log(balancePara("{[}"))
+
+//End of question one 
+
+
+
+// Question 2 (Stack)
+
+class stack {
+    constructor() {
+        this.count = 0
+        this.storage = {}
+    }
+    push(n) {
+        this.storage[this.count] = n
+        this.count++
+        return n
+    }
+    pop() {
+        if (this.count === 0) {
+            return "No value to pop"
+        }
+        this.count--
+        let delItem = this.storage[this.count]
+        delete this.storage[this.count]
+        return delItem
+    }
+    max() {
+        if (this.count === 0) {
+            return "No value to pop"
+        }
+        let maximum = this.storage[0]
+        for (let i in this.storage) {
+            if (this.storage[i] > maximum) {
+                maximum = this.storage[i]
+            }
+        }
+        return maximum
+    }
+}
+
+// a = new stack()
+// console.log(a.push(3))
+// console.log(a.push(1))
+// console.log(a.push(2))
+// console.log(a.max())
+// console.log(a.pop())
+// console.log(a)
+
+// End of question 2
